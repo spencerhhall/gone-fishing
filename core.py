a837ee74fd456fff39b778ea290b0ac5efff0e74
spencerhh <spencer.hall16@gmail.com>
2019-06-24 17:15:10 -0700

    Created new blank modules

---
+++ core.py
@@ -0,0 +1,7 @@
+# Module Functionality
+# Run file and have option to view existing trips, create  new trip, or exit (loop)
+# This file will contain the main looping
+# Trips will have the trip and event classes
+# Create has the functions for creating new events and trips
+
+import create
